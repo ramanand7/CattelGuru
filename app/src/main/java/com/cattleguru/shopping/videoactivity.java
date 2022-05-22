a23009b8dfcf35486ab553a545556b743469d725
sadhna88059 <sadhnasharma41234@gmail.com>
2022-05-22 17:03:12 +0800

    new changes

---
+++ app/src/main/java/com/cattleguru/shopping/videoactivity.java
@@ -0,0 +1,49 @@
+package com.cattleguru.shopping;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class videoactivity extends AppCompatActivity {
+    RecyclerView recview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_videoactivity);
+        recview=(RecyclerView)findViewById(R.id.recview);
+        recview.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<filemodel> options =
+                new FirebaseRecyclerOptions.Builder<filemodel>()
+                        .setQuery(FirebaseDatabase.getInstance().getReference().child("myvideos"), filemodel.class)
+                        .build();
+
+
+        FirebaseRecyclerAdapter<filemodel,myviewholder> firebaseRecyclerAdapter=new FirebaseRecyclerAdapter<filemodel, myviewholder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull myviewholder holder, int position, @NonNull filemodel model) {
+                holder.prepareexoplayer(getApplication(),model.getTitle(),model.getVurl());
+            }
+
+            @NonNull
+            @Override
+            public myviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.singelrow,parent,false);
+                return new myviewholder(view);
+            }
+        };
+
+        firebaseRecyclerAdapter.startListening();
+        recview.setAdapter(firebaseRecyclerAdapter);
+    }
+}
